32bc39a78b025648e4884bec0e6ca73d18db0984
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2024-08-10 05:09:10 +0100

    Added BookingsRepository class

---
+++ src/main/java/com/example/rental_system/repository/BookingsRepository.java
@@ -0,0 +1,9 @@
+package com.example.rental_system.repository;
+
+import com.example.rental_system.entity.Bookings;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingsRepository extends JpaRepository<Bookings, Long> {
+}
